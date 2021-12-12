d7b1011b143e20cc7ef11822915f9c466853bdda
ummetduran <ummetduran@outlook.com>
2021-12-13 00:00:35 +0300

    Add new user done.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,33 +2,61 @@ import React from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 import UserService from '../services/userService';
 import { toast } from "react-toastify";
+import { Route, Router } from 'react-router'
+import ProductList from '../pages/ProductList'
+
 export default function SignIn() {
+  const user= {}
+
+  const routeChange = () => {
+
+      <Route exact path = "/" element={<ProductList/>}/>  
+
+   
+      
+  
+  }
+  const handleAddUser = () => {
+    
+    console.log("user => ", user)
+
+    let userService = new UserService()
+    userService.addUser(user)
+    toast.success(`${user.fullName} kayıt edildi!`)
+    routeChange()
+  }
+  const handleChange = (event) => {
+    
+    if(event.target.name =='fullName'){
+        user.fullName = event.target.value
+    }else{
+      user.email = event.target.value
+    }
+    
 
   
-  const handleAddUser = (user) => {
-    let userService = UserService()
-    userService.addUser(user)
-    toast.success(`${user.userName} kayıt edildi!`)
+    console.log(user)
+    console.log(event.target.name, event.target.value)
   }
     return (
-      
-        <Form>
-          <Form.Field fluid>
+        
+        <Form onSubmit={handleAddUser}>
+          <Form.Field>
             <label position="left">User Name</label>
-            <input name ="userName" placeholder="User Name" />
+            <input name ="fullName" value = {user.fullName} onChange={handleChange} placeholder="User Name" />
           </Form.Field>
           <Form.Field>
             <label>Email</label>
-            <input  name ="email" placeholder="Email" />
+            <input  name ="email" value = {user.email} onChange={handleChange} placeholder="Email" />
           </Form.Field>
           <Form.Field>
             <label>Password</label>
-            <input  name ="password" placeholder="Password" />
+            <input  name ="password" placeholder="Password" type = "password"/>
           </Form.Field>
           <Form.Field>
             <Checkbox label="I agree to the Terms and Conditions" />
           </Form.Field>
-          <Button onclick = {() => handleAddUser({username,email})} type="submit">Submit</Button>
+          <Button type="submit">Submit</Button>
         </Form>
       
     );
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,36 @@
 import axios from "axios";
-export default class UserService{
-    addUser(props){
-        return axios({
-            method:'post',
-            url:"http://localhost:4444/user",
-            headers:{},
-            data:{props}
+export default class UserService {
+  // addUser(props){
+  //     return axios({
+  //         method:'post',
+  //         url:"http://localhost:4444/user",
+  //         headers:{},
+  //         data:{props}
 
-        })
-    }
+  //     })
+  // }
+  addUser(props) {
+    var axios = require("axios");
+    var data = JSON.stringify({
+      fullName: props.fullName,
+      email: props.email,
+    });
+
+    var config = {
+      method: "post",
+      url: "http://localhost:4444/user",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
